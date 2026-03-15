5d5f79100267b7eda2cd2a18c66ff6abdaa43d0c
kiranPandey <priyapandeykiran@gmail.com>
2026-03-16 00:58:43 +0530

    add the map style url

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
      container : "map", // container ID
+     style : "mapbox://styles/mapbox/streets-v11", // style URL
      center: coordinate, // starting position [lng, lat]. Note that lat must be set between -90 and 90
      zoom: 9 // starting zoom
 });
